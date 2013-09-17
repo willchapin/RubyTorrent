3a5ae93acf7f67e64da3f78aa35fb4cec4b3c57f
willchapin <wrchapin@gmail.com>
2013-09-16 20:57:58 -0400

    clean up debugging puts'

--- new.rb
+++ new.rb
@@ -18,18 +18,17 @@ puts my_cli.meta_info["info"]
 
 interested = "\0\0\0\1\2"
 peer.connection.write(interested)
+
 puts "unchoke"
 puts peer.connection.read(5).bytes
 
 
 offset = 0
 data = ""
-puts "piecebytes"
-puts my_cli.meta_info["info"]["pieces"].bytes[0...20]
-puts "piecebytes end"
+puts 'piece length'
 puts my_cli.meta_info["info"]["piece length"]
 
-while offset <  my_cli.meta_info["info"]["piece length"] 
+while offset <=  my_cli.meta_info["info"]["piece length"] - 2**14 
   
   #puts data.bytes.length
   msg_length = "\0\0\0\x0d"
@@ -40,7 +39,7 @@ while offset <  my_cli.meta_info["info"]["piece length"]
   
   #puts byte_offset.inspect
   
-  byte_offset.force_encoding("utf-8")
+  #byte_offset.force_encoding("utf-8")
   
   #puts piece_index.encoding
   #puts byte_offset.encoding
@@ -49,30 +48,33 @@ while offset <  my_cli.meta_info["info"]["piece length"]
   request = msg_length + id + piece_index + byte_offset + request_length
   
   puts request.inspect
-  puts offset  
+  puts offset/(2**14)  
 
   peer.connection.write(request)
-  puts 'total length'
   length = peer.connection.read(4)
   temp = length.unpack("N")[0]
-  break if length == nil || temp == 0
-  puts temp
-  puts 'p_length'
+  #break if length == nil || temp == 0
+  #puts temp
+  #puts 'p_length'
+  puts "temp: " + temp.to_s
   p_length = temp - 9
-  puts p_length
-  puts 'id'
-  puts peer.connection.read(1).bytes
-  puts 'index'
-  puts peer.connection.read(4).unpack("N")
-  puts 'begin'
-  puts peer.connection.read(4).unpack("N")
-  puts 'block'
+  #puts p_length
+  #puts 'id'
+  id = peer.connection.read(1).bytes
+  #puts 'index'
+  p_index = peer.connection.read(4).unpack("N")
+  #puts 'begin'
+  block_offset = peer.connection.read(4).unpack("N")
+  #puts 'block'
   data << peer.connection.read(p_length)
-  puts 'stop'
+  #puts 'stop'
+  puts offset
+  puts block_offset
   offset += p_length
 end
 
 puts data.length
 puts Digest::SHA1.new.digest(data).bytes
+puts '-----------------------'
 puts my_cli.meta_info["info"]["pieces"].bytes[0...20]
 puts Digest::SHA1.new.digest(data) == my_cli.meta_info["info"]["pieces"][0...20]
