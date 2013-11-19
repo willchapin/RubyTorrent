2e318bfb804d12f6476c51d869cc0418b10f53b3
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-11-19 18:17:43 -0500

    Stop Block knowing about Helpers or metainfo
    * Helpers is a big bag that Block only used one fn from.
    * Just pass in piece_length instead of using the function from Helpers

--- incoming_message_process.rb
+++ incoming_message_process.rb
@@ -5,63 +5,63 @@ class IncomingMessageProcess
     @message_queue = message_queue
     @metainfo = metainfo
   end
-  
+
   def run!
     loop do
       process_message(@message_queue.pop)
     end
   end
-  
+
   def process_message(message)
     puts "Peer #{message.peer.id} has sent you a #{message.type} message"
     send(message.type, message)
   end
-  
+
   def keep_alive(message)
   end
-  
+
   def choking(message)
     message.peer.state[:is_choking] = true
   end
-  
+
   def unchoke(message)
     message.peer.state[:is_choking] = false
   end
-  
+
   def not_interested(message)
     message.peer.state[:is_interested] = false
   end
-  
+
   def interested(message)
     message.peer.state[:is_interested] = true
   end
-  
+
   def have(message)
     puts "have"
     message.peer.bitfield.have_piece(message.payload.unpack("N")[0])
     puts message.peer.bitfield
   end
-  
+
   def bitfield(message)
   end
-  
+
   def request(message)
   end
-  
+
   # A piece is really a block, not a whole piece.
   def piece(message)
     piece_index, byte_offset, block_data = split_piece_payload(message.payload)
     puts @metainfo.class
-    @incoming_block_queue.push(Block.new(piece_index, byte_offset, block_data, @metainfo))
+    @incoming_block_queue.push(Block.new(piece_index, byte_offset, block_data, @metainfo.piece_length))
   end
-  
+
   def cancel(message)
   end
-  
+
   # needed for DHT implementation
   def port(message)
   end
-    
+
   def split_piece_payload(payload)
     piece_index = payload.slice!(0..3).unpack("N")[0]
     byte_offset = payload.slice!(0..3).unpack("N")[0]
