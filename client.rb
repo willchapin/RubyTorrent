79981cae4a8078104ef6fb4b51ba44cee9355774
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-11-19 18:10:31 -0500

    Whitespace

--- client.rb
+++ client.rb
@@ -1,5 +1,5 @@
 class Client
-    
+
   def initialize(path_to_file)
     @torrent = File.open(path_to_file)
     @message_queue = Queue.new
@@ -22,7 +22,7 @@ class Client
   def send_tracker_request
     @tracker.make_request(get_tracker_request_params)
   end
-  
+
   def tracker_request_params
     { info_hash:    @metainfo.info_hash,
       peer_id:      self.class.rand_id,
@@ -34,17 +34,17 @@ class Client
       no_peer_id:   '0',
       event:        'started' }
   end
-  
+
   def set_peers
     peers = @tracker.make_request(tracker_request_params)["peers"].scan(/.{6}/)
     peers.map! do |peer|
       peer.unpack('a4n')
     end
     peers.each do |ip_string, port|
-      set_peer(ip_string, port)   
+      set_peer(ip_string, port)
     end
   end
-  
+
   def set_peer(ip_string, port)
     begin
       Timeout::timeout(1) { @peers << Peer.new(ip_string, port, @handshake, @metainfo.info_hash) }
@@ -54,23 +54,23 @@ class Client
   end
 
   def run!
-    
+
     Thread::abort_on_exception = true # remove later?
-    Thread.new { IncomingMessageProcess.new(@message_queue, @incoming_block_queue, @metainfo).run! } 
-    Thread.new { DownloadController.new(@metainfo, @block_request_queue, @incoming_block_queue, @peers).run! } 
+    Thread.new { IncomingMessageProcess.new(@message_queue, @incoming_block_queue, @metainfo).run! }
+    Thread.new { DownloadController.new(@metainfo, @block_request_queue, @incoming_block_queue, @peers).run! }
     Thread.new { BlockRequestProcess.new(@block_request_queue).run! }
-    
+
     @peers.each do |peer|
       Thread.new { Message.parse_stream(peer, @message_queue) }
       Thread.new { keep_alive(peer) }
       Message.send_interested(peer) # change later
     end
   end
-  
+
   def join_threads
     Thread.list.each { |thread| thread.join unless current_thread?(thread) }
   end
-  
+
   def keep_alive(peer)
     loop do
       begin
@@ -81,7 +81,7 @@ class Client
       sleep(60)
     end
   end
-  
+
   def current_thread?(thread)
     thread == Thread.current
   end
